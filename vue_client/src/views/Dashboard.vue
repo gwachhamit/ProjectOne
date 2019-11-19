331843a68e80b45d447fd559abcde0a28a417b86
amit gwachha <genuineaametax@gmail.com>
2019-11-19 13:12:28 +0545

    Fix: some issue in index.html client

--- vue_client/src/views/Dashboard.vue
+++ vue_client/src/views/Dashboard.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="dashboard">
         <section class="content-header">
-            <h1>Dashboard</h1>     
+            <h1> <i class="fa fa-tachometer-alt"></i> Dashboard</h1>     
         </section>
     </div>
 </template>
